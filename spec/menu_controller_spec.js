d757816cbb5b3eedf035df6f2335c3d84f929215
Tish <latisha.griffiths@gmail.com>
2018-11-05 21:37:05 -0600

    Completed assignment: testing

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -9,14 +9,20 @@
    describe("#getContactCount()", () => {
 
      it("should return 0 when no contacts are in the book", () => {
-       const menu = new MenuController();
        expect(this.menu.getContactCount()).toBe(0);
      });
 
-    it("should return 1 when there is exactly one contact in the book", () => {
-      const menu = new MenuController();
-      this.menu.contacts.push("Bob");
-      expect(this.menu.getContactCount()).toBe(1)
+      it("should return 1 when there is exactly one contact in the book", () => {
+        this.menu.contacts.push("Bob");
+        expect(this.menu.getContactCount()).toBe(1)
+      });
+
+    describe("#remindMe()", () => {
+
+      it("should return a string containing the text, 'Learning is a life-long pursuit.'", () => {
+        expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit.");
+      });
+
     });
    });
  });
