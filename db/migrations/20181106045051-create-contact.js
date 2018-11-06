f9ed952ab18e48594cddcb0fc10dc00b22ce19b2
Tish <latisha.griffiths@gmail.com>
2018-11-06 01:13:17 -0600

    completed checkpoint: Creating Contacts and Connecting to a Database

---
+++ db/migrations/20181106045051-create-contact.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+//creates a table
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+//drops the contacts table
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Contacts');
+  }
+};
